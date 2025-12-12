5fd18789104bc66e0bc5d26d13320659d1b432ea
greenslopesdirect-commits <greenslopesdirect@gmail.com>
2025-12-12 12:57:46 +1000

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,7 @@ import { Helmet } from "react-helmet";
 import SEOHead from "@/components/SEOHead";
 import Hero from "@/components/Hero";
 import NewsletterSection from "@/components/NewsletterSection";
-import WeatherInfo from "@/components/WeatherInfo";
+import WeatherWidget from "@/components/WeatherWidget";
 import QuickAccess from "@/components/QuickAccess";
 import TopPicks from "@/components/TopPicks";
 import SeoContentSections from "@/components/SeoContentSections";
@@ -65,7 +65,7 @@ const Index = () => {
         <NewsletterSection />
         
         <section id="weather">
-          <WeatherInfo />
+          <WeatherWidget />
         </section>
         <section id="quick-access">
           <QuickAccess />
