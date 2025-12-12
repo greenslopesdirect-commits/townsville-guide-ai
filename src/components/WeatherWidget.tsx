3b661cd8fe40a4d871e5eec5d575f66fab37250d
greenslopesdirect-commits <greenslopesdirect@gmail.com>
2025-12-12 12:51:49 +1000

    Update WeatherWidget.tsx

--- src/components/WeatherWidget.tsx
+++ src/components/WeatherWidget.tsx
@@ -23,18 +23,21 @@ const WeatherWidget = () => {
   }, []);
 
   return (
-    <div className="my-6 flex justify-center" ref={widgetRef}>
-      <a
-        className="weatherwidget-io"
-        href="https://forecast7.com/en/n19d26146d82/townsville/"
-        data-label_1="TOWNSVILLE"
-        data-label_2="WEATHER"
-        data-theme="original"
-      >
-        TOWNSVILLE WEATHER
-      </a>
-    </div>
-  );
-};
+  <div
+    className="my-6 flex justify-center border border-red-500"
+    ref={widgetRef}
+  >
+    <a
+      className="weatherwidget-io"
+      href="https://forecast7.com/en/n19d26146d82/townsville/"
+      data-label_1="TOWNSVILLE"
+      data-label_2="WEATHER"
+      data-theme="original"
+    >
+      TOWNSVILLE WEATHER
+    </a>
+  </div>
+);
+
 
 export default WeatherWidget;
